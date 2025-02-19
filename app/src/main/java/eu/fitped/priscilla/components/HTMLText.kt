9242201b5fbdb2d3b6f7cb6050e263e561fa94fb
Nevermind3005 <jakub.elis007@gmail.com>
2025-02-19 15:04:05 +0100

    kotlin compiler extension update, changes to HTMLText style

--- app/src/main/java/eu/fitped/priscilla/components/HTMLText.kt
+++ app/src/main/java/eu/fitped/priscilla/components/HTMLText.kt
@@ -8,20 +8,16 @@ import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.viewinterop.AndroidView
-import kotlinx.coroutines.launch
-import org.json.JSONArray
+
 
 @SuppressLint("RememberReturnType", "SetJavaScriptEnabled")
 @Composable
@@ -48,6 +44,8 @@ fun WebViewWithInput(
                     ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT
                 )
+
+                setPadding(0, 0, 0, 0);
 
                 setBackgroundColor(backgroundColorARGB)
 
@@ -103,18 +101,63 @@ private fun handleHtmlTemplate(
     backgroundColor: String,
     textColor: String,
 ): String {
-    html.trimIndent()
-    var newHtml = "<!DOCTYPE html><html><head><style>*{ background-color:${backgroundColor} !important; color:${textColor} !important; -webkit-user-select: none !important; }body{height:100%;word-wrap:break-word;}img{max-width: 100%;}</style></head><body>"
-    newHtml += html
+    val processedHtml = html.trimIndent().replace("§§_§§", "<input type=\"text\" style='fit-content' class=\"answer\" size=\"1\"/>")
 
-    while (newHtml.contains("§§_§§")) {
-        newHtml = newHtml.replaceFirst("§§_§§", "<input type=\"text\" class=\"answer\" size=\"1\"/>")
-    }
-    newHtml += "<script>const getAnswers = () => {\n" +
-            "    const inputs = document.getElementsByClassName('answer');\n" +
-            "    const output = [...inputs].map((i) => i.value);\n" +
-            "    return output;\n" +
-            "};</script></body></html>"
-    Log.i("HandleHtmlTemplate", newHtml)
-    return newHtml
+    // Script to get answers from web view
+    val answerScript = """
+        const getAnswers = () => {
+            const inputs = document.getElementsByClassName('answer');
+            const output = [...inputs].map((i) => i.value);
+            return output;
+        }
+    """.trimIndent()
+
+    val baseStyle = """
+        * {
+            max-width: 100% !important;
+            box-sizing: border-box !important;
+            background-color:${backgroundColor} !important; 
+            color:${textColor} !important; 
+            -webkit-user-select: none !important;
+        }
+        body {
+            height:100%;
+        }
+        img {
+            max-width: 100%;
+        }
+        html, body {
+            margin: 0 !important;
+            padding: 0 !important;
+            width: 100% !important;
+            overflow-x: hidden !important;
+        }
+        pre {
+            white-space: pre-wrap !important;
+            margin: 0 !important;
+            padding: 8px !important;
+            overflow-x: hidden !important;
+        }
+    """.trimIndent()
+
+    val htmlDoc = """
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <style>
+                    $baseStyle
+                </style>
+            </head>
+            <body>
+                $processedHtml
+                <script>
+                    $answerScript
+                </script
+            </body>
+        </html>
+    """.trimIndent()
+
+    Log.i("HandleHtmlTemplate", htmlDoc)
+
+    return htmlDoc
 }
