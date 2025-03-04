cf29f1937b76aea5d4d5f6de8592edfc79660777
Nevermind3005 <jakub.elis007@gmail.com>
2025-03-04 16:44:05 +0100

    add lesson switching

--- app/src/main/java/eu/fitped/priscilla/screen/ChapterDetail.kt
+++ app/src/main/java/eu/fitped/priscilla/screen/ChapterDetail.kt
@@ -4,10 +4,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import eu.fitped.priscilla.components.LessonList
 import eu.fitped.priscilla.model.LessonDto
 import eu.fitped.priscilla.util.DataState
 import eu.fitped.priscilla.viewModel.ChapterDetailViewModel
@@ -19,15 +18,14 @@ fun ChapterDetail(
 ) {
     val chapterDetailViewModel: ChapterDetailViewModel = hiltViewModel()
     val state by chapterDetailViewModel.dataState.collectAsState()
-    val currentLesson by remember {
-        mutableIntStateOf(0)
-    }
     if (chapterId != null) {
         when (state) {
             is DataState.Loading -> Loading()
             is DataState.Success<*> -> {
-                navController.popBackStack()
-                navController.navigate("LESSON_TASKS/${(state as DataState.Success<LessonDto>).data.course.id}/${(state as DataState.Success<LessonDto>).data.chapter.id}/${(state as DataState.Success<LessonDto>).data.lessonDetailList.first().lessonId}")
+                LessonList(
+                    lessonDto = (state as DataState.Success<LessonDto>).data,
+                    navController = navController
+                )
             }
             is DataState.Error -> Text("Error: ${(state as DataState.Error).message}")
         }
