ca70716400dae0a4d81adb3ef55477121de1b4ad
Nevermind3005 <jakub.elis007@gmail.com>
2025-03-18 17:11:06 +0100

    replace AreaCard and AreaList with generic version

--- app/src/main/java/eu/fitped/priscilla/screen/CourseCategoryDetail.kt
+++ app/src/main/java/eu/fitped/priscilla/screen/CourseCategoryDetail.kt
@@ -7,8 +7,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import eu.fitped.priscilla.components.AreaList
+import eu.fitped.priscilla.components.core.GenericList
+import eu.fitped.priscilla.components.core.SimpleCard
 import eu.fitped.priscilla.model.CourseCategoryAreasDto
+import eu.fitped.priscilla.navigation.Screen
 import eu.fitped.priscilla.util.DataState
 import eu.fitped.priscilla.viewModel.CourseCategoryDetailViewModel
 
@@ -27,11 +29,18 @@ fun CourseCategoryDetail(
             is DataState.Loading -> Loading()
             is DataState.Success<*> ->  {
                 val data = (state as DataState.Success<CourseCategoryAreasDto>).data
-                AreaList(
-                    navController = navController,
-                    categoryName = data.category.name,
-                    courseCategoryAreaInfo = data.areas
-                )
+                GenericList(
+                    headerText = data.category.name,
+                    items = data.areas,
+                    itemKey = { it.id }
+                ) {
+                    SimpleCard(
+                        cardText = it.areaName,
+                        onClick = {
+                            navController.navigate("${Screen.AREA_DETAIL.name}/${it.id}")
+                        }
+                    )
+                }
             }
             is DataState.Error -> Text("Error: ${(state as DataState.Error).message}")
         }
