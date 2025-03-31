790cda5e22ee7ce0263fc8a1237b57d3f8c876f7
Nevermind3005 <jakub.elis007@gmail.com>
2025-03-31 15:23:16 +0200

    replace LessonList with generic generic version

--- app/src/main/java/eu/fitped/priscilla/screen/ChapterDetail.kt
+++ app/src/main/java/eu/fitped/priscilla/screen/ChapterDetail.kt
@@ -4,9 +4,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import eu.fitped.priscilla.components.LessonList
+import eu.fitped.priscilla.R
+import eu.fitped.priscilla.components.LessonCard
+import eu.fitped.priscilla.components.core.GenericList
 import eu.fitped.priscilla.model.LessonDto
 import eu.fitped.priscilla.util.DataState
 import eu.fitped.priscilla.viewModel.ChapterDetailViewModel
@@ -22,10 +25,19 @@ fun ChapterDetail(
         when (state) {
             is DataState.Loading -> Loading()
             is DataState.Success<*> -> {
-                LessonList(
-                    lessonDto = (state as DataState.Success<LessonDto>).data,
-                    navController = navController
-                )
+                val data = (state as DataState.Success<LessonDto>).data
+                GenericList(
+                    headerText = stringResource(R.string.lessons),
+                    items = data.lessonDetailList,
+                    itemKey = { it.lessonId }
+                ) {
+                    LessonCard(
+                        lessonDetail = it,
+                        onClick = {
+                            navController.navigate("LESSON_TASKS/${data.course.id}/${data.chapter.id}/${it.lessonId}")
+                        }
+                    )
+                }
             }
             is DataState.Error -> Text("Error: ${(state as DataState.Error).message}")
         }
