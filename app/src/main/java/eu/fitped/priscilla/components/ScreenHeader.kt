b8382c40e42d37dff6c105d5ff5c3357b109d608
Nevermind3005 <jakub.elis007@gmail.com>
2025-03-31 15:12:25 +0200

    remake ChapterList using generic component

--- app/src/main/java/eu/fitped/priscilla/components/ScreenHeader.kt
+++ app/src/main/java/eu/fitped/priscilla/components/ScreenHeader.kt
@@ -4,15 +4,19 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -40,5 +44,35 @@ fun ScreenHeader(
         }
     ) { padding ->
         content(padding)
+    }
+}
+
+@Composable
+fun FullHeader(text: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp, vertical = 8.dp),
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.ExtraBold
+        )
+    }
+}
+
+@Composable
+fun CompactHeader(text: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp, vertical = 8.dp),
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = FontWeight.ExtraBold
+        )
     }
 }
--- app/src/main/java/eu/fitped/priscilla/screen/CourseDetail.kt
+++ app/src/main/java/eu/fitped/priscilla/screen/CourseDetail.kt
@@ -1,14 +1,15 @@
 package eu.fitped.priscilla.screen
 
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import eu.fitped.priscilla.components.ChapterList
+import eu.fitped.priscilla.components.ChapterCard
+import eu.fitped.priscilla.components.core.GenericList
 import eu.fitped.priscilla.model.CourseDetailDto
+import eu.fitped.priscilla.navigation.Screen
 import eu.fitped.priscilla.util.DataState
 import eu.fitped.priscilla.viewModel.CourseDetailViewModel
 
@@ -23,13 +24,22 @@ fun CourseDetail(
     if (courseId != null) {
         when (state) {
             is DataState.Loading -> Loading()
-            is DataState.Success<*> -> ChapterList(
-                chapterList = (state as DataState.Success<CourseDetailDto>).data.chapterList,
-                title = (state as DataState.Success<CourseDetailDto>).data.title,
-                navController = navController
-            )
+            is DataState.Success<*> -> {
+                val data = (state as DataState.Success<CourseDetailDto>).data
+                GenericList(
+                    headerText = data.title,
+                    items = data.chapterList,
+                    itemKey = { it.chapterId }
+                ) {
+                    ChapterCard(
+                        chapterDto = it,
+                        onClick = {
+                            navController.navigate("${Screen.CHAPTER_DETAIL.name}/${it.chapterId}")
+                        }
+                    )
+                }
+            }
             is DataState.Error -> Text("Error: ${(state as DataState.Error).message}")
         }
-
     }
 }
